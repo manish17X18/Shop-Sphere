74b9c1e5962728eb3005244260ac9c082e6e77c4
manish17x18 <manishdesh1710@gmail.com>
2026-05-17 18:23:31 +0530

    store

---
+++ src/store/store.jsx
@@ -0,0 +1,11 @@
+import { configureStore } from "@reduxjs/toolkit";
+import ProductThunk from '../features/products/ProjectThunk'
+import CartStore from '../features/cart/cartSlice'
+import SignUpUser from '../features/login/SignUp'
+export const store=configureStore({
+    reducer:{
+        fetchProducts:ProductThunk,
+        cartProducts:CartStore,
+        signIn:SignUpUser
+    }
+})
