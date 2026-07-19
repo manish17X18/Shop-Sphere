93de3ccf4efa8abeed72f06a6dc7623e81f37a0b
manish17x18 <manishdesh1710@gmail.com>
2026-07-19 10:24:34 +0530

    Added user Id in Schema

--- backend/models/Cart.js
+++ backend/models/Cart.js
@@ -1,6 +1,10 @@
 const mongoose=require('mongoose');
 
 const CartSchema=new mongoose.Schema({
+    user:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"User"
+    },
     item:{
         type:mongoose.Schema.Types.ObjectId,
         ref:"Product"
